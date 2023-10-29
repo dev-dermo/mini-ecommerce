8166569dfe2b6dd1e80121f3c7e3743c65f0d708
diarmuid-murphy <diarmuid.com@gmail.com>
2023-10-29 16:01:00 -0700

    cleanup

--- controllers/html/index.js
+++ controllers/html/index.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { Product } = require('../../models');
 const categoryRoutes = require('./categoryRoutes');
 const productRoutes = require('./productRoutes');
 const homeRoutes = require('./homeRoutes');
